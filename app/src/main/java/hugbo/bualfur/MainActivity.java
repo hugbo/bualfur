538b8c30f58c3f93b0fd1009a0c6fd5d7eb8a479
egill <egill.ian1@gmail.com>
2017-03-19 15:42:58 +0000

    Facebook SDK support

--- app/src/main/java/hugbo/bualfur/MainActivity.java
+++ app/src/main/java/hugbo/bualfur/MainActivity.java
@@ -4,6 +4,7 @@ import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import com.facebook.FacebookSdk;
 
 public class MainActivity extends SingleFragmentActivity {
 
