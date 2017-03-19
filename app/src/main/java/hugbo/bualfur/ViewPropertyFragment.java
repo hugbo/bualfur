0c50f4a19de42fcb3c3c732df14aeb67443f7bdb
Stefan Carl Peiser <scg2@hi.is>
2017-03-19 21:48:38 +0000

    ViewProperty updates

--- app/src/main/java/hugbo/bualfur/ViewPropertyFragment.java
+++ app/src/main/java/hugbo/bualfur/ViewPropertyFragment.java
@@ -30,9 +30,9 @@ public class ViewPropertyFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
-        Bundle args = getArguments();
-        id = args.getString("id", "");
-
+//        Bundle args = getArguments();
+//        id = args.getString("id", "1");
+        id = "1";
         return inflater.inflate(R.layout.fragment_view_property, container, false);
 
     }
@@ -41,31 +41,53 @@ public class ViewPropertyFragment extends Fragment {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setRetainInstance(true);
-        fetchProperty();
+        PropertyFetcher.getInstance(getActivity()).searchProperties(null, new ServerCallback() {
+            @Override
+            public void onSuccess(ArrayList<Property> results) {
+                fetchProperty();
+            }
+        });
     }
 
     private void fetchProperty() {
         propertyToView = PropertyFetcher.getInstance(getActivity()).getProperty(id);
 
         try {
-            TextView AddressTextView = (TextView) this.getView().findViewById(R.id.property_address);
-            TextView PriceTextView = (TextView) this.getView().findViewById(R.id.property_price);
-            TextView ZipcodeTextView = (TextView) this.getView().findViewById(R.id.property_zipcode);
-            TextView CityTextView = (TextView) this.getView().findViewById(R.id.property_city);
-            TextView SizeTextView = (TextView) this.getView().findViewById(R.id.property_size);
-            TextView BedroomsTextView = (TextView) this.getView().findViewById(R.id.property_bedrooms);
-            TextView BathroomsTextView = (TextView) this.getView().findViewById(R.id.property_bathrooms);
-            TextView TypeTextView = (TextView) this.getView().findViewById(R.id.property_houseType);
+            TextView addressTextView = (TextView) this.getView().findViewById(R.id.property_address);
+            TextView priceTextView = (TextView) this.getView().findViewById(R.id.property_price);
+            TextView zipcodeTextView = (TextView) this.getView().findViewById(R.id.property_zipcode);
+            TextView cityTextView = (TextView) this.getView().findViewById(R.id.property_city);
+            TextView sizeTextView = (TextView) this.getView().findViewById(R.id.property_size);
+            TextView bedroomsTextView = (TextView) this.getView().findViewById(R.id.property_bedrooms);
+            TextView bathroomsTextView = (TextView) this.getView().findViewById(R.id.property_bathrooms);
+            TextView typeTextView = (TextView) this.getView().findViewById(R.id.property_houseType);
             //TextView DescriptionTextView = (TextView) this.getView().findViewById(R.id.property_description);
 
-            AddressTextView.setText(propertyToView.getmAddress());
-            PriceTextView.setText(propertyToView.getmPrice());
-            ZipcodeTextView.setText(propertyToView.getmZipcode());
-            CityTextView.setText(propertyToView.getmCity());
-            SizeTextView.setText(propertyToView.getmSize());
-            BedroomsTextView.setText(propertyToView.getmNumBedrooms());
-            BathroomsTextView.setText(propertyToView.getmNumBathrooms());
-            TypeTextView.setText(propertyToView.getmPropertyType());
+            String address = propertyToView.getmAddress();
+
+            addressTextView.setText(address);
+
+            String price = String.valueOf(propertyToView.getmPrice());
+
+            priceTextView.setText(price);
+
+            String zipcode = String.valueOf(propertyToView.getmZipcode());
+            zipcodeTextView.setText(zipcode);
+
+            String city = propertyToView.getmCity();
+            cityTextView.setText(city);
+
+            String size = String.valueOf(propertyToView.getmSize());
+            sizeTextView.setText(size);
+
+            String numBedrooms = String.valueOf(propertyToView.getmNumBedrooms());
+            bedroomsTextView.setText(numBedrooms);
+
+            String numBathrooms = String.valueOf(propertyToView.getmNumBathrooms());
+            bathroomsTextView.setText(numBathrooms);
+
+            String propertyType = propertyToView.getmPropertyType();
+            typeTextView.setText(propertyType);
             //DescriptionTextView.setText(propertyToView.getm);
         } catch (NullPointerException e) {
             Log.e("ViewProperty", e.toString());
