227a7ddd400edb09e6acc4f5dc6818ba8fcb81e0
Oddgeir Georgsson <doddip123@gmail.com>
2017-03-19 17:32:32 +0000

    View property fragment layout basic version ready

---
+++ app/src/main/java/hugbo/bualfur/ViewPropertyFragment.java
@@ -0,0 +1,26 @@
+package hugbo.bualfur;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Oddgeir on 18.3.2017.
+ */
+
+public class ViewPropertyFragment implements android.support.v4.app.Fragment {
+
+    private Property propertyToView;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+        return null;
+
+    }
+
+}
